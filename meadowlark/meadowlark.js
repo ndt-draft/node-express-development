8e4a4aa81623a15ffa93d9d37e296d92fb27181a
thanh4890 <thanh4890@gmail.com>
2018-08-27 15:30:27 +0700

    dynamic content in views

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,5 +1,13 @@
 var express = require('express')
 
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+];
+
 var app = express()
 
 app.set('port', process.env.PORT || 3000)
@@ -14,7 +22,8 @@ app.get('/', function (req, res) {
 })
 
 app.get('/about', function (req, res) {
-  res.render('about')
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
 })
 
 app.use(express.static('public'))
